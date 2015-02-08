3007ab108684dc124224e88e8f391d97c39173b8
anton-johansson <antoon.johansson@gmail.com>
2015-02-08 12:04:09 +0100

    Fetch company and person addresses separately

--- javascript/webshop/app/modules/address-lookup/AddressLookupController.js
+++ javascript/webshop/app/modules/address-lookup/AddressLookupController.js
@@ -2,17 +2,10 @@ var Klarna = require('klarna-nodejs')
 
 var AddressLookupController = function(router, models)
 {
-	router.get('/address-lookup/:identifier', function(request, response)
+	router.get('/address/person/:identifier', function(request, response)
 	{
-		var parameters =
-		{
-			eid: 123,
-			sharedSecret: '...',
-			address: 'https://payment.testdrive.klarna.com:443'
-		};
-
-		var klarna = new Klarna(parameters);
-		klarna.getAddresses(request.params.identifier, function(addresses)
+		var identifier = request.params.identifier;
+		getAddresses(identifier, false, function(addresses)
 		{
 			response.status(200);
 			response.json(addresses);
@@ -21,6 +14,48 @@ var AddressLookupController = function(router, models)
 			response.status(400);
 			response.json(error);
 		});
+	});
+
+	router.get('/address/company/:identifier', function(request, response)
+	{
+		var identifier = request.params.identifier;
+		getAddresses(identifier, true, function(addresses)
+		{
+			response.status(200);
+			response.json(addresses);
+		}, function(error)
+		{
+			response.status(400);
+			response.json(error);
+		});
+	});
+}
+
+function getAddresses(identifier, isCompany, success, failure)
+{
+	var parameters =
+	{
+		eid: 123,
+		sharedSecret: '...',
+		address: 'https://payment.testdrive.klarna.com:443'
+	};
+
+	var klarna = new Klarna(parameters);
+	klarna.getAddresses(identifier, function(addresses)
+	{
+		var output = [];
+		addresses.forEach(function(address)
+		{
+			if (address.isCompany === isCompany)
+			{
+				delete address.isCompany;
+				output.push(address);
+			}
+		});
+		success(output);
+	}, function(error)
+	{
+		failure(error);
 	});
 }
 
